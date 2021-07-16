3d6d42e6eafccf303150014fb2a1de8ba73078fb
mauricio muñoz corredor <arnoldmunoz@ustadistancia.edu.co>
2021-07-16 09:31:50 -0500

    commit inicial

--- src/main/java/com/springboot/data/jpa/spring/bootdata/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/springboot/data/jpa/spring/bootdata/app/models/dao/ClienteDaoImpl.java
@@ -2,16 +2,23 @@ package com.springboot.data.jpa.spring.bootdata.app.models.dao;
 
 import com.springboot.data.jpa.spring.bootdata.app.models.entity.Cliente;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 
 @Repository
-public class ClienteDaoImpl implements IClienteDao{
+public class ClienteDaoImpl implements IClienteDao {
+
+    @PersistenceContext
+    private EntityManager em; //se encarga de manejar todas las entidades y administrar CRUD a la  BD
 
 
     @Override
+    @Transactional(readOnly = true)//como es una consulta se pone de solo lectura
     public List<Cliente> findAll() {
-        return null;
+        return em.createQuery("from Cliente").getResultList();
     }
 }
