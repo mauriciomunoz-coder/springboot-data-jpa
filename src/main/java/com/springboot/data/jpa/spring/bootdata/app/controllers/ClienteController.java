3bc84ff8bbaf5396c9135d0880955f73944212df
mauricio muñoz corredor <arnoldmunoz@ustadistancia.edu.co>
2021-07-16 12:14:14 -0500

    creacion mensajes de error en el formulario

--- src/main/java/com/springboot/data/jpa/spring/bootdata/app/controllers/ClienteController.java
+++ src/main/java/com/springboot/data/jpa/spring/bootdata/app/controllers/ClienteController.java
@@ -6,9 +6,11 @@ import com.springboot.data.jpa.spring.bootdata.app.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -29,14 +31,19 @@ public class ClienteController {
     @GetMapping(value = "/form")
     public String crear(Map<String, Object> model) {
         Cliente cliente = new Cliente();
-        model.put("cliente", cliente);
+        model.put("cliente", cliente);  //se pone para mapear el objeto al formulario
         model.put("titulo", "Formulario de Cliente");
 
         return "form";
     }
 
     @PostMapping(value = "/form")
-    public String guardar(Cliente cliente){
+    public String guardar(@Valid Cliente cliente, BindingResult result,Model model){
+        if (result.hasErrors()){
+            model.addAttribute("titulo", "Formulario de Cliente");
+            return "form";
+        }
+
         IClienteDao.save(cliente);
         return "redirect:listar";
     }
--- src/main/java/com/springboot/data/jpa/spring/bootdata/app/models/entity/Cliente.java
+++ src/main/java/com/springboot/data/jpa/spring/bootdata/app/models/entity/Cliente.java
@@ -1,8 +1,13 @@
 package com.springboot.data.jpa.spring.bootdata.app.models.entity;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -16,10 +21,18 @@ public class Cliente implements Serializable {
 
     //@Column(name = "nombres")  //cambia el nombre en la tabla de BD
 
+    @NotEmpty
+    @Size(min = 4, max = 20) //valida que el nombre este entre 4 a 12 caracteres
     private String nombre;
+
+    @NotEmpty
     private String apellido;
+
+    @NotEmpty
+    @Email
     private String email;
 
+    @NotNull
     @Column(name = "create_at")  //cambia el nombre en la tabla de BD
     //@Temporal(TemporalType.DATE)//indica el formato en el que se va a guardar la fecha en la BD
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -72,7 +85,6 @@ public class Cliente implements Serializable {
     }
 
     private static final long serialVersionUID = 1L;
-
 
 
 }
