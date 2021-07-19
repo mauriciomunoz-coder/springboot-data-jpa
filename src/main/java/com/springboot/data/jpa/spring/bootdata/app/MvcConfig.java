304cb3df1e3a55212fb64508af5df7cfa964a36f
mauricio muñoz corredor <arnoldmunoz@ustadistancia.edu.co>
2021-07-19 15:06:17 -0500

    agregando foto a directorio absoluto y externo en raiz del proyecto

--- src/main/java/com/springboot/data/jpa/spring/bootdata/app/MvcConfig.java
+++ src/main/java/com/springboot/data/jpa/spring/bootdata/app/MvcConfig.java
@@ -4,14 +4,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // TODO Auto-generated method stub
         WebMvcConfigurer.super.addResourceHandlers(registry);
 
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:/C:/Temp/uploads/"); //uploads es la misma ruta que pusimos en la vista ver.html para que busque la foto
+        String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+        log.info(resourcePath);
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(resourcePath);
     }
 }
