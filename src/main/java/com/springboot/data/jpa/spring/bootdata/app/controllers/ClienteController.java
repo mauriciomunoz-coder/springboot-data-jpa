76eed773ea406edcaf7b21ec195844ce44f8bc07
mauricio muñoz corredor <arnoldmunoz@ustadistancia.edu.co>
2021-07-21 09:49:47 -0500

    creando la interfaz IUploadFileService para manejar todo lo relacionado con mostrar la imagen y cargarla

--- src/main/java/com/springboot/data/jpa/spring/bootdata/app/controllers/ClienteController.java
+++ src/main/java/com/springboot/data/jpa/spring/bootdata/app/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.springboot.data.jpa.spring.bootdata.app.controllers;
 
 import com.springboot.data.jpa.spring.bootdata.app.models.entity.Cliente;
 import com.springboot.data.jpa.spring.bootdata.app.models.service.IClienteService;
+import com.springboot.data.jpa.spring.bootdata.app.models.service.IUploadFileService;
 import com.springboot.data.jpa.spring.bootdata.app.util.paginator.PageRender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -42,17 +43,21 @@ public class ClienteController {
     @Autowired
     private IClienteService iClienteService;
 
-    //**** este es un metodo alternativo para subir la imagen sin tener que usar la clase MvcConfig.java ***********************
+
+    // inyectamos la dependencia de iuploadFileService
+    @Autowired
+    private IUploadFileService iuploadFileService;
+
+
+    //**** este es un metodo alternativo para ver la imagen sin tener que usar la clase MvcConfig.java ***********************
 
     @GetMapping("/uploads/{filename:.+}") //es la misma ruta que tiene el campo para img de la vista ver
     public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
-        Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+
         Resource recurso = null;
+
         try {
-            recurso = new UrlResource(pathFoto.toUri());  //carga la imagen para mostrarla en la vista
-            if (!recurso.exists() || !recurso.isReadable()) {
-                throw new RuntimeException("Error: No se puede cargar la imagen! " + pathFoto.toString());
-            }
+            recurso = iuploadFileService.load(filename);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -118,37 +123,25 @@ public class ClienteController {
         //******************** codigo para subir imagenes *******************************************************************
 
         if (!foto.isEmpty()) {
- // codigo que elimina la foto anterior cuando se cambia la foto o elimina el cliente  **********************************************************
+            // codigo que elimina la foto anterior cuando se cambia la foto o elimina el cliente  **********************************************************
             if (cliente.getId() != null && cliente.getId() > 0 && cliente.getFoto() != null && cliente.getFoto().length() > 0) {
-                Path rootPath = Paths.get("uploads").resolve(cliente.getFoto()).toAbsolutePath();
-                File archivo = rootPath.toFile();
 
-                if (archivo.exists() && archivo.canRead()) {
-                    archivo.delete();
-                }
+                iuploadFileService.delete(cliente.getFoto());
             }
-            //********* fin codigo que elimina la imagen cuando se cambia la foto o elimina el cliente*************************************************
 
-
-            String uniqueFilename = UUID.randomUUID().toString() + "_" + foto.getOriginalFilename();
-            Path rootPath = Paths.get("uploads").resolve(uniqueFilename);
-
-            Path rootAbsolutPath = rootPath.toAbsolutePath();
-
-            log.info("rootPath: " + rootPath);
-            log.info("rootAbsolutPath: " + rootAbsolutPath);
+            String uniqueFilename = null;
 
             try {
-
-                Files.copy(foto.getInputStream(), rootAbsolutPath);
-
-                flash.addFlashAttribute("info", "Has subido correctamente '" + uniqueFilename + "'");
-
-                cliente.setFoto(uniqueFilename);
-
+                uniqueFilename = iuploadFileService.copy(foto);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            flash.addFlashAttribute("info", "Has subido correctamente '" + uniqueFilename + "'");
+
+            cliente.setFoto(uniqueFilename);
+            //********* fin codigo que elimina la imagen cuando se cambia la foto o elimina el cliente*************************************************
+
         }
         //********************************** fin codigo para cargar foto o imagen ********************************************************************************************************
         String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito!" : "Cliente creado con éxito!"; //verifica si el cliente ya existe o es nuevo y dependiendo saca el mensaje
@@ -187,19 +180,18 @@ public class ClienteController {
     @GetMapping(value = "/delete/{id}")
     public String delete(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
         if (id > 0) {
-            Cliente cliente = iClienteService.findOne(id);
+
             iClienteService.delete(id);
             flash.addFlashAttribute("success", "Cliente Eliminado con Exito");
 
-            Path rootPath = Paths.get("uploads").resolve(cliente.getFoto()).toAbsolutePath();
-            File archivo = rootPath.toFile();
-
-            if (archivo.exists() && archivo.canRead()) {
-                if (archivo.delete()) {
-                    flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + "eliminada con exito!");
-                }
+            //***** codigo para eliminar la foto cuando se cambie la imagen o se elimine el cliente **************************
+            Cliente cliente = iClienteService.findOne(id);
+            if (iuploadFileService.delete(cliente.getFoto())) {
+                flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + "eliminada con exito!");
             }
+            // ******************************* fin del codigo eliminar imagen ************************************************
         }
+
         return "redirect:/listar";
     }
 
---
+++ src/main/java/com/springboot/data/jpa/spring/bootdata/app/models/service/IUploadFileService.java
@@ -0,0 +1,16 @@
+package com.springboot.data.jpa.spring.bootdata.app.models.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public interface IUploadFileService {
+
+    public Resource load(String filename) throws MalformedURLException; //muestra la imagen
+
+    public String copy(MultipartFile file) throws IOException; //regresa el nombre de la nueva imagen
+
+    public boolean delete(String filename); //regresa true o false para saber si elimino o no la imagen
+}
